28d1810b2fe9b84ae2a8fcebfb1cd29ae848f8c4
Adrian Cubillos <adriancubillos@gmail.com>
2025-06-13 11:35:45 -0400

    sing in and sign up forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
-import "./globals.css";
+import React from 'react';
+import type { Metadata } from 'next';
+import { Mona_Sans } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'sonner';
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
+  variable: '--font-mona-sans',
+  subsets: ['latin'],
 });
 
-
 export const metadata: Metadata = {
-  title: "Prep Wizard",
-  description: "AI powered platform for preparing mock Interviews",
+  title: 'Prep Wizard',
+  description: 'AI powered platform for preparing mock Interviews',
 };
 
 export default function RootLayout({
@@ -19,11 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased pattern`}
-      >
+    <html
+      lang="en"
+      className="dark">
+      <body className={`${monaSans.className} antialiased pattern`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
